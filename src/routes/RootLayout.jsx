3883a6d63e7fdbe7c86c3c15aa17844d9a6d1005
KritikaSharmaKS <sh.kritika01@gmail.com>
2024-03-07 14:50:04 -0500

    add styling to RootLayout and MainNavigation components

--- src/routes/RootLayout.jsx
+++ src/routes/RootLayout.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom"
 import MainNavigation from "../components/MainNavigation"
+import classes from './RootLayout.module.css'
 
 export default function RootLayout () {
     return (
         <>
             <MainNavigation />
-            <Outlet />
+            <main className={classes.content}>
+                <Outlet />
+            </main>
         </>
     )
 }
