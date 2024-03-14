552f263208e15843327a998f7049f9343d2b587d
KritikaSharmaKS <sh.kritika01@gmail.com>
2024-03-13 21:14:13 -0400

    showing error page with errorElement + update styling

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
 import {
   createBrowserRouter,
   RouterProvider,
-  createRoutesFromElements,
-  Route,
 } from "react-router-dom";
 import Homepage from "./routes/Homepage";
 import ProductsPage from "./routes/ProductsPage";
 import RootLayout from "./routes/RootLayout";
-
-// const routeDefinitions = createRoutesFromElements(
-//   <Route>
-//     <Route path="/" element={<Homepage />}/>
-//     <Route path="/products" element={<ProductsPage />}/>
-//   </Route>
-// )
+import ErrorPage from "./routes/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <Homepage /> },
       { path: "/products", element: <ProductsPage /> },
     ],
   },
 ]);
-
-// const router = createBrowserRouter(routeDefinitions)
 
 function App() {
   return <RouterProvider router={router} />;
