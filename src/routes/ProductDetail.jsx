f1dfbfe59f4dbdecf94dfa3e79163c8b8af21e92
KritikaSharmaKS <sh.kritika01@gmail.com>
2024-03-13 21:41:48 -0400

    useParams hook for dynamic routing

--- src/routes/ProductDetail.jsx
+++ src/routes/ProductDetail.jsx
@@ -1,5 +1,11 @@
+import { useParams } from "react-router-dom"
+
 export default function ProductDetail () {
+    const params = useParams();
     return (
-        <h1>Product Details Page!</h1>
+        <>
+            <h1>Product Details Page!</h1>
+            <p>{params.id}</p>
+        </>
     )
 }
